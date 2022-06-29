1670feb0937ca00dfff2e42ca390da6af6f0341d
Edward Todd <edwardtdd@gmail.com>
2022-06-29 13:55:12 +0100

    a

--- src/Components/Pages/AdoptPage.js
+++ src/Components/Pages/AdoptPage.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import AnimalCards from '../AnimalCards/AnimalCards'
+import './AdoptPage.css'
 
 function AdoptPage() {
   return (
-    <div>
-        <AnimalCards />
-    </div>
+    <>
+      <h1 className='adoptTitle'>
+        Adopt
+      </h1>
+      <AnimalCards />
+    </>
   )
 }
 
