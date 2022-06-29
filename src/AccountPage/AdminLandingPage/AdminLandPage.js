97669699667cc79fdda0821e01124266868b9b00
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-06-29 19:27:09 +0100

    Finished Basic Org

--- src/AccountPage/AdminLandingPage/AdminLandPage.js
+++ src/AccountPage/AdminLandingPage/AdminLandPage.js
@@ -1,15 +1,31 @@
-import ApplicationAdminCont from "./PostOrgUser Container/ApplicationAdmin/ApplicationAdminCont"
-import OrgAdminCont from "./PostOrgUser Container/OrgAdmin/OrgAdminCont"
-import PostAdminCont from "./PostOrgUser Container/PostAdmin/PostAdminCont"
+import ApplicationAdminCont from "./PostOrgUser Container/ApplicationAdmin/ApplicationAdminCont.js";
+import OrgAdminCont from "./PostOrgUser Container/OrgAdmin/OrgAdminCont.js";
+import PostAdminCont from "./PostOrgUser Container/PostAdmin/PostAdminCont.js";
+import UserAdminCont from "./PostOrgUser Container/UserAdmin/UserAdminCont.js";
 
+import "./AdminLandPageBasic.css";
+import "./AdminLandPageTablet+.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaw } from "@fortawesome/free-solid-svg-icons";
 
 const AdminLandPage = () => {
   return (
     <>
-        <main>
-            <PostAdminCont />
+        <main id="AdminLandPage">
+
+          <h2 className="AdminWelcome">Welcome, Admin User <FontAwesomeIcon icon={faPaw} /></h2>
+
+          <div className="Split-Admin-AppPost">
             <ApplicationAdminCont />
+            <PostAdminCont />
+          </div>
+
+          <div className="Split-Admin-UserOrg">
+            <UserAdminCont />
             <OrgAdminCont />
+          </div>
+ 
         </main>
     </>
   )
