cf44db53c7b60f5e90d10dc9e22510bd03025d67
Edward Todd <edwardtdd@gmail.com>
2022-06-29 13:48:37 +0100

    Installed mui

--- src/Components/AnimalCards/AnimalCards.js
+++ src/Components/AnimalCards/AnimalCards.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import AnimalCardItem from './AnimalCardItem'
+import { Grid, TableContainer } from '@mui/material'
+import { useEffect, useState } from 'react'
 
-function AnimalCards() {
+const AnimalCards = () => {
+ 
+  const animalArray = [
+    {name: "a"},
+    {name: "b"},
+    {name: "c"},
+    {name: "d"},
+    {name: "e"},
+  ]
+
+  const[cards, setCards] = useState(animalArray);
+
   return (
-    <div className='animalcards'>
-        <div className='animalcards_container'>
-            <ul>
-                <AnimalCardItem 
-                src={process.env.PUBLIC_URL + '/familydog.jpeg'}
-                />
-            </ul>
-        </div>
-    </div>
+      <Grid container spacing={2} columns={3}>
+          {cards && cards.map(card => <Grid item>
+            <AnimalCardItem src={process.env.PUBLIC_URL + '/familydog.jpeg'}/>
+            </Grid>)}
+      </Grid>
   )
 }
 
