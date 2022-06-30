7531bd984eea03f7d3b3ce98d0d819183cb0139e
Rebecca Bartholomew <cerulean.rebecca20@hotmail.com>
2022-06-30 16:43:24 +0100

    altered home css

--- src/HomePage/Home.js
+++ src/HomePage/Home.js
@@ -10,26 +10,26 @@ function Home() {
             <div className='centred'>Say Hello to Your New Best Friend</div>
         </div> */}
 
-        <section className='c-hero'>
+        <section className='hero'>
 
-          <div className='c-shape'>
+          <div className='hero-shape'>
             <div className='hero-video'>
-              <video autoPlay muted loop poster='https://woodgreen.org.uk/wp-content/uploads/2022/05/tokyo-summer-banner-img.jpg' id="html5_video_gc9ibputb4" src="https://woodgreen.org.uk/wp-content/uploads/2022/05/tokyo-summer-banner-1.mp4">
+              <video autoPlay muted loop poster='https://woodgreen.org.uk/wp-content/uploads/2022/05/tokyo-summer-banner-img.jpg' id="html5_video_gc9ibputb4" src="https://cdn.videvo.net/videvo_files/video/free/2021-08/large_watermarked/210804_01_Dog%20Walk_4k_029_preview.mp4">
               </video>
             </div>
           </div>
 
-          <div className='c-hero__content-container'>
-            <div className='c-hero-content'>
-              <h1 className='c-hero-title'>Your new best friend is waiting for you!</h1>
-              <h2 className='c-hero-subtitle h4'>
+          <div className='hero__content-container'>
+            <div className='hero-content'>
+              <h1 className='hero-title'>Your new best friend is waiting for you!</h1>
+              <h2 className='hero-subtitle'>
                 You never know who you'll meet and bond with.
                 <br></br>
                 Because you can't choose the perfect pet.
                 <br></br>
                 You find each other.
               </h2>
-              <div className='c-hero-adopt-button'>
+              <div className='hero-adopt-button'>
                 <a href='http://localhost:3000/adopt' className='hero-button'>Adopt today!</a>
               </div>
             </div>
