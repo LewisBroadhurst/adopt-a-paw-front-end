588ac6e00771eddf40912d5bf8eb169bc78acbc2
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-07-01 10:39:01 +0100

    Mobile Create Acc Page

--- src/CreateAccountPage/CreateAccPage.js
+++ src/CreateAccountPage/CreateAccPage.js
@@ -1,4 +1,8 @@
+import { faPaw } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./CreateAccPage.css";
+
+import gpMobForm from "./images/gpMobForm.jpeg";
 
 const CreateAccPage = () => {
 
@@ -6,41 +10,41 @@ const CreateAccPage = () => {
     <>
         <main className="cap__main--positioning">
             <p>This is your first step to taking a happy Animal home!</p>
-            <p>Thank you, from us, our partner organisations, and most importantly, <span>our animals</span></p>
+            <p>Thank you, from us, our partner organisations, and most importantly, our animals <FontAwesomeIcon icon={faPaw} /></p>
 
             <section className="cap__imgCont--positioning">
-                <img className="" src="" alt="happy doge" />
+                <img className="" src={gpMobForm} alt="happy doge" />
             </section>
 
             <form className="cap__form--positioning">
                 <div className="cap__form--id">
                     <label>ID</label>
-                    <input type="text"></input>
+                    <input type="text" placeholder="ID number"></input>
                 </div>
 
                 <div className="cap__form--firstName">
                     <label>First Name</label>
-                    <input type="text"></input>
+                    <input type="text" placeholder="First Name"></input>
                 </div>
 
                 <div className="cap__form--lastName">
                     <label>Last Name</label>
-                    <input type="text"></input>
+                    <input type="text" placeholder="Last Name"></input>
                 </div>
 
                 <div className="cap__form--age">
                     <label>Age</label>
-                    <input type="text"></input>
+                    <input type="text" placeholder="Age"></input>
                 </div>
 
                 <div className="cap__form--location">
                     <label>Location</label>
-                    <input type="text"></input>
+                    <input type="text" placeholder="Location"></input>
                 </div>
 
                 <div className="cap__form--prevAdopted">
                     <label>Previously Adopted?</label>
-                    <input type="text"></input>
+                    <input type="text" placeholder="Adopted before?"></input>
                 </div>
 
                 <button type="submit">Create Account</button>
