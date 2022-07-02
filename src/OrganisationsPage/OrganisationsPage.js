830663dfb7acff6b837a1dfc46dd5090f6ade4db
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-07-02 16:47:58 +0100

    Org improved 2

--- src/OrganisationsPage/OrganisationsPage.js
+++ src/OrganisationsPage/OrganisationsPage.js
@@ -1,4 +1,8 @@
 import "./OrganisationsPage.css";
+import OrganisationCardContainer from "./OrganisationCardContainer/OrganisationCardContainer.js";
+import rspca_1 from "./images/rspca_1.jpeg";
+import dogs_trust1 from "./images/dogs_trust1.jpeg";
+import battersea_1 from "./images/battersea_1.jpeg";
 
 
 const OrganisationsPage = () => {
@@ -21,15 +25,52 @@ const OrganisationsPage = () => {
   
           </section>
 
-          <form className="orgPage__searchBar--pos">
-            <input type="text" placeholder="Enter Organisation Name"></input>
-            <input type="text" placeholder="Enter Keyword"></input>
-            <button>Search</button>
-          </form>
-
           <section className="orgPage__orgList--pos">
 
-            
+            <section className="orgPage__orgContV1">
+              <h2>RSPCA</h2>
+              <p>A world where all animals are respected and treated with kindness and compassion.</p>
+
+              <p>We're the Royal Society for the Prevention of Cruelty to Animals (RSPCA) and we've been here for animals since 1824. We're the world's oldest and largest animal welfare charity, with the primary focus of rescuing, rehabilitating and rehoming or releasing animals across England and Wales.</p> 
+              
+              <p>Every day, with the help of our people and our generous supporters, we take steps to achieve our mission.</p>
+            </section>
+
+            <div className="OP__imgCont--pos">
+              <img src={rspca_1} alt="" />
+            </div>
+
+            <div className="OP__imgCont--pos">
+              <img src={dogs_trust1} alt="" className="OP_DT"/>
+            </div>
+
+            <section className="orgPage__orgContV1">
+              <h2>Dogs Trust</h2>
+
+              <p>Today, many dog owners think of their pet as a member of the family. But when we were founded in the nineteenth century, attitudes were very different.</p>
+
+              <p>For most people, dogs were there to do a job. Cruel practices, such as prolonged chaining, were common.</p>
+
+              <p>In 1891, Lady Gertrude Stock started the National Canine Defence League. Her small group vowed to campaign against cruelty.</p>
+
+              <p>In the decades that followed, the League continued to campaign for better dog welfare. It increased rehoming, survived two world wars and worked with partners overseas.</p>
+
+              <p>We became Dogs Trust in 2003. Since then, we’ve celebrated many successes, from helping to change laws to launching our Dog School.</p>
+
+              <p>We’re the UK’s leading dog welfare charity  – and we’re proud of the many milestones in our history. You can read more about them in our timeline below.</p>
+            </section>
+
+            <section className="orgPage__orgContV1">
+              <h2>Battersea</h2>
+
+              <p>From the moment we welcomed our first stray dog in 1860 we have been placing our animals at the centre of everything we do. More than three million animals later, we're still working hard to achieve our vision that every dog and cat should live in a home where they are treated with love, care and respect.</p>
+
+              <p>Battersea aims to never turn away a dog or cat in need of help, caring for them until their owners or loving new homes can be found, no matter how long it takes. We are champions for, and supporters of, vulnerable dogs and cats, determined to create lasting changes for animals in our society.</p>
+            </section>
+
+            <div className="OP__imgCont--pos">
+              <img src={battersea_1} alt="" />
+            </div>
 
           </section>
 
