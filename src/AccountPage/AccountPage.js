9339d9ee2d1eb2ce791b344394bf1deff084dec0
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-07-02 22:23:18 +0100

    updated account landing page

--- src/AccountPage/AccountPage.js
+++ src/AccountPage/AccountPage.js
@@ -13,25 +13,28 @@ const AccountPage = () => {
 
   return (
     <>
-        <main id="Account-Main-Page">
-            <section className="Animal-Image-Acc">
-                <img src="https://placedog.net/600/600" alt="" />
-            </section>
+        <section className="ap__background--color">
+            <h2>Join us, and begin your adoption journey...</h2>
+            <main id="Account-Main-Page">
+                <section className="LogIn-Section">
+                    <h3>Adopt A Paw <FontAwesomeIcon icon={faPaw} /> </h3>
 
-            <section className="LogIn-Section">
-                <h3>Adopt A Paw <FontAwesomeIcon icon={faPaw} /> </h3>
+                    <form>
+                        <input type="text" placeholder="Email Address" />
+                        <input type="text" placeholder="Password" />
+                        <button type="submit" onClick={handleLogIn}><Link to="/account/admin">Log In</Link></button>
+                    </form>
 
-                <form>
-                    <input type="text" placeholder="Email Address" />
-                    <input type="text" placeholder="Password" />
-                    <button type="submit" onClick={handleLogIn}><Link to="/account/admin">Log In</Link></button>
-                </form>
+                    <span className="Forgot-Password-Acc">Forgot your password?</span>
 
-                <span className="Forgot-Password-Acc">Forgot your password?</span>
+                    <button className="New-Acc-BTN" type="button"><Link to="/account/createAccount">Create New Account</Link></button>
+                </section>
 
-                <button className="New-Acc-BTN" type="button"><Link to="/account/createAccount">Create New Account</Link></button>
-            </section>
-        </main>
+                <section className="Animal-Image-Acc">
+                    <img src="https://placedog.net/600/600" alt="" />
+                </section>
+            </main>
+        </section>
     </>
   )
 }
