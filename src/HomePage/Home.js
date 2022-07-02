02852f4c262aef17b3a28df77ba18478864028e5
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-07-02 11:08:29 +0100

    Improved homepage

--- src/HomePage/Home.js
+++ src/HomePage/Home.js
@@ -1,15 +1,15 @@
+import { faPaw } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react'
 import './Home.css';
 import DisplayCarousel from "./HomepageCarousel/DisplayCarousel";
+import wwf_logo from "./images/wwf_logo.jpeg";
+import rspca_logo from "./images/rspca_logo.png";
+import battersea_gif from "./images/battersea.gif";
 
 function Home() {
   return (
-    <div>
-        {/* <div className='homepage-image'>
-            <img src={process.env.PUBLIC_URL + '/familydog.jpeg'} alt="xyz"/>
-            <div className='centred'>Say Hello to Your New Best Friend</div>
-        </div> */}
-
+    <>
         <section className='hero'>
 
           <div className='hero-shape'>
@@ -36,11 +36,44 @@ function Home() {
           </div>
         </section>
 
+        <section className='homePage__ourMission--container'>
+
+          <span>Our Mission</span>
+
+          <p>At Adopt A Paw, we believe all animals have the right to live in a happy home, with a loving family.</p>
+          <p>This is why we work with reputable shelters to rehome the wonderful animals they have to offer.</p>
+          <p>Our goal is to match these beautiful animals with caring owners (you!), so they can live a life of joy <FontAwesomeIcon icon={faPaw} /></p>
+
+
+        </section>
+
+        <section className='homePage__ourPartners--container'>
+
+          <span>Our Partners</span>
+
+          <p>We only work with the best in the industry. The RSPCA, WWF, Battersea Cats & Dogs Shelter, and more, are all close partners who trust us with their rehoming needs.</p>
+
+          <section className='homePage__parentLogoCont'>
+            <div className='homePage__logoContainer'>
+              <img src={wwf_logo} alt='' />
+            </div>
+
+            <div className='homePage__logoContainer'>
+              <img src={battersea_gif} alt='' />
+            </div>
+
+            {/* <div className='homePage__logoContainer'>
+              <img src={rspca_logo} alt='' />
+            </div> */}
+          </section>
+        
+        </section>
+
 
         <div className='display_carousel'>
         <DisplayCarousel />
         </div>
-    </div>
+    </>
   )
 }
 
