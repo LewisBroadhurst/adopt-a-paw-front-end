7502d793cfb07222d886a3856f36b0ed903c8c23
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-07-03 14:33:41 +0100

    Updated customer page

--- src/AccountPage/UserLandingPage/UserLandPage.js
+++ src/AccountPage/UserLandingPage/UserLandPage.js
@@ -3,7 +3,7 @@ import AnimalList from "../AdminLandingPage/PostOrgUser Container/AnimalAdmin/An
 import "./UserLandPage.css";
 import { getAllAnimals, getAllApplications } from "../../API";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
+import { faArrowUp, faPaw, faStar } from "@fortawesome/free-solid-svg-icons";
 
 const UserLandPage = () => {
 
@@ -13,7 +13,18 @@ const UserLandPage = () => {
     useEffect( () => {
         getAllAnimals(setAnimals);
         getAllApplications(setApplications)
-    }, [])
+    }, []);
+
+    const toggleWishlistStar = () => {
+        const star = document.querySelector(".ulp__starCont");
+
+        if (star.style.color === "yellow") {
+            star.style.color = "white";
+        } else {
+            star.style.color = "yellow";
+        }
+        
+    }
 
   return (
     <>
@@ -31,7 +42,7 @@ const UserLandPage = () => {
                             applications.map( (app, index) => {
                                 return <li key={index}>{app.id} {app.customer.firstName} {app.customer.lastName}
                                                     {app.customer.application.map((application, index) => {
-                                                        return <li>{application.animal.name}, {application.animal.location}</li>
+                                                        return <li key={index}>{application.animal.name}, {application.animal.location}</li>
                                                     })}</li>
                             })
                         }
@@ -39,13 +50,23 @@ const UserLandPage = () => {
                 </section>
 
                 <section className="ulp__center">
-                    <h2>Available Animals!</h2>
+                    <h2>Available Animals <FontAwesomeIcon icon={faPaw} /></h2>
                     <ul className="ulp__animalList">
                     {
                         animals.map( (animal, index) => {
-                            return <li key={index}>{animal.name}, {animal.location}, {animal.breed}, {animal.sex}, {animal.dateOfBirth}, {animal.organisation.name}
-                                        {animal.name}, {animal.location}, {animal.breed}, {animal.sex}, {animal.dateOfBirth}, {animal.organisation.name}
-                                        {animal.name}, {animal.location}, {animal.breed}, {animal.sex}, {animal.dateOfBirth}, {animal.organisation.name}</li>
+                            return  <li key={index} className="ulp__animalCont">
+                                            <section className="ulp__animalCont__text">
+                                                <h3>{animal.name}    <span onClick={toggleWishlistStar}><FontAwesomeIcon icon={faStar}  className="ulp__starCont"/></span></h3>
+                                                <span>Location: {animal.location}</span>
+                                                <span>Date of birth: {animal.dateOfBirth}</span>
+                                                <span>Specie & Breed: {animal.species}, {animal.breed}</span>
+                                                <span>Adoption status: {animal.availableStatus}</span>
+                                            </section>
+
+                                            <section className="ulp__animalCont__img">
+                                                <img src="https://placedog.net/250/250" alt="dog" />
+                                            </section>
+                                    </li>
                         })
                     }
                     </ul>
@@ -53,8 +74,11 @@ const UserLandPage = () => {
 
                 <section className="ulp__rightSideBar">
                     <section className="ulp__form">
-                        <button><FontAwesomeIcon icon={faArrowDown} /></button>
-                        <h3>Animal Wishlist</h3>
+                        <div className="ulp__form__header">
+                            <h3>Animal Wishlist</h3>
+                            <button><FontAwesomeIcon icon={faArrowUp} /></button>
+                        </div>
+                        
                         <ul>
                             <li>A1</li>
                             <li>A1</li>
@@ -65,8 +89,10 @@ const UserLandPage = () => {
                     </section>
 
                     <section className="ulp__form">
-                        <button><FontAwesomeIcon icon={faArrowDown} /></button>
-                        <h3>Apply to Adopt!</h3>
+                        <div className="ulp__form__header">
+                            <h3>Apply to Adopt</h3>
+                            <button><FontAwesomeIcon icon={faArrowUp} /></button>
+                        </div>
                         <form>
                             <input type="text"></input>
                             <input type="text"></input>
@@ -76,8 +102,11 @@ const UserLandPage = () => {
                     </section>
 
                     <section className="ulp__form">
-                        <button><FontAwesomeIcon icon={faArrowDown} /></button>
-                        <h2>Change your details</h2>
+                        <div className="ulp__form__header">
+                            <h3>Change your details</h3>
+                            <button><FontAwesomeIcon icon={faArrowUp} /></button>
+                        </div>
+                        
                         <form>
                             <input type="text"></input>
                             <input type="text"></input>
