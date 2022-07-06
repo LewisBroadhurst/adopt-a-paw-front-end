8e37be25bb3b5e0412a86078569fa488d9b16711
Cem Gurhan <cemg@live.co.uk>
2022-07-06 12:22:57 +0100

    user registration done on create account page

--- src/CreateAccountPage/CreateAccPage.js
+++ src/CreateAccountPage/CreateAccPage.js
@@ -5,15 +5,19 @@ import Header from "../Header/Header";
 import Footer from "../Footer/Footer"
 
 import gpMobForm from "./images/gpMobForm.jpeg";
-import { addNewCustomer } from "../API";
+import { addNewCustomer, RegisterNewUser } from "../API";
 import { useState } from "react";
 
 const CreateAccPage = () => {
 
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
-    const [age, setAge] = useState(0);
-    const [location, setLocation] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [userRole, setUserRole] = useState('');
+    const [showPassword, setShowPassword] = useState("password");
+    // const [age, setAge] = useState(0);
+    // const [location, setLocation] = useState('');
     // const [prevAdopted, setPrevAdopted] = useState(false);
 
 
@@ -21,20 +25,70 @@ const CreateAccPage = () => {
     const handleCreateNewCustomer = event => {
         event.preventDefault();
 
-        console.log(age);
+        // console.log(age);
 
-        const custData = {
-            "id": 0,
-            "firstName": `${firstName}`,
-            "lastName": `${lastName}`,
-            "age": `${age}`,
-            "location": `${location}`,
-            "previousAdoptions": false
-        }
+        // const custData = {
+        //     "id": 0,
+        //     "firstName": `${firstName}`,
+        //     "lastName": `${lastName}`,
+        //     "age": `${age}`,
+        //     "location": `${location}`,
+        //     "previousAdoptions": false
+        // }
 
-        addNewCustomer(custData);
+        // addNewCustomer(custData);
 
         alert("Successfully created a new account! You will now be re-directed to the sign in page.")
+    }
+
+
+    const registerUser = event => {
+
+      event.preventDefault();
+
+      const request = {
+
+        "firstName": `${firstName}`,
+        "lastName": `${lastName}`,
+        "email" : `${email}`,
+        "password" : `${password}`,
+        "appUserRole" : `${userRole}`
+
+
+      }
+      
+      RegisterNewUser(request);
+
+
+      alert("Successfully created a new account! You will now be re-directed to the sign in page.")
+
+
+
+
+    }
+
+    const handleShowPassWordClick = () => {
+
+        if(showPassword === "password"){
+
+          setShowPassword("text");
+
+        }else if(showPassword === "text"){
+
+          setShowPassword("password")
+
+        }
+
+    }
+
+    const handleSelectRole = (e) => {
+
+      setUserRole(e.target.value);
+
+
+
+
+
     }
 
 
@@ -78,24 +132,51 @@ const CreateAccPage = () => {
                     <input type="text" placeholder="Last Name" onChange={(e) => setLastName(e.target.value)}></input>
                 </div>
 
-                <div className="cap__form--age">
-                    {/* <label>Age</label> */}
+                <div className="cap__form--lastName">
+                    {/* <label>Last Name</label> */}
+                    <input type="text" placeholder="Email" onChange={(e) => setEmail(e.target.value)}></input>
+                </div>
+
+                <div className="password__input">
+                    {/* <label>Last Name</label> */}
+                    <input type={`${showPassword}`} placeholder="Password" onChange={(e) => setPassword(e.target.value)}></input>
+                </div>
+                <label>Show password</label>
+                <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" onClick={()=>{handleShowPassWordClick()}}></input>
+
+
+
+                {/* <div className="cap__form--lastName">
+                    <label>Last Name</label>
+                    <input type="text" placeholder="Role" onChange={(e) => setUserRole(e.target.value)}></input>
+                </div> */}
+                <label className="whoAreYouLabel">Who are you?</label>
+                <select className="userRolesDropdown" name="cars" onChange={handleSelectRole}>
+                  <option value="USER">USER</option>
+                  <option value="ORGANISATION">ORGANISATION</option>
+                  <option value="ADMIN">ADMIN</option>
+                </select>
+                
+              
+
+                {/* <div className="cap__form--age">
+                    <label>Age</label>
                     <input type="text" placeholder="Age" onChange={(e) => setAge(parseInt(e.target.value))}></input>
                 </div>
 
                 <div className="cap__form--location">
-                    {/* <label>Location</label> */}
+                    <label>Location</label>
                     <input type="text" placeholder="Location" onChange={(e) => setLocation(e.target.value)}></input>
                 </div>
 
                 <div className="cap__form--prevAdopted">
-                    {/* <label>Previously Adopted?</label> */}
+                    <label>Previously Adopted?</label>
                     <input type="text" placeholder="Adopted before?"></input>
-                </div>
+                </div> */}
 
                 <br></br>
 
-                <button type="submit" onClick={handleCreateNewCustomer}>Create Account</button>
+                <button type="submit" onClick={registerUser}>Create Account</button>
 
             </form>
 
